61a09ac66ca4777846ea95962301e8ff31d84850
flipmode@wp.tv <flipmode@wp.tv>
2025-09-08 13:28:51 +0100

    eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,3 @@
-// eslint.config.js
-export default [
-  {
-    files: ['**/*.js'],
-    rules: {
-      semi: ['error', 'always'],
-      quotes: ['error', 'single'],
-      // add other rules here
-    },
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-    },
-  },
+module.exports = [
+  // your config
 ];
