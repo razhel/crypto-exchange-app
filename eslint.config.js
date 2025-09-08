5edef7b6d97cd668ec78a9b6a7c0827c84f8d7cc
flipmode@wp.tv <flipmode@wp.tv>
2025-09-08 13:31:18 +0100

    eslint cicd

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,23 @@
-module.exports = [
-  // your config
-];
+module.exports = {
+  env: {
+    browser: false,
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:node/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-unused-vars': 'warn',         // Warn if variables are declared but not used
+    'no-console': 'off',              // Allow console.log etc.
+    'eqeqeq': ['error', 'always'],    // Require === and !==
+    'curly': 'error',                 // Require braces for blocks
+    'semi': ['error', 'always'],      // Require semicolons
+    'quotes': ['error', 'single'],    // Use single quotes
+  },
+};
